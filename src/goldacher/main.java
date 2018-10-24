9c1e015c537d4ba05c601e8c26f983d9574aa5e0
yanivbaldi <yaniv-b@campus.technion.ac.il>
2018-10-24 12:20:01 +0300

    main

--- src/goldacher/main.java
+++ src/goldacher/main.java
@@ -10,11 +10,10 @@ public class main {
 		ArithIter itrEven =  new ArithIter(BigInteger.valueOf(0), BigInteger.valueOf(2));
 		while(itrEven.hasNext()) {
 			BigInteger even = itrEven.next();
-			PrimeIter itrPrime =  new PrimeIter();
+			System.out.println(even);
+			/*PrimeIter itrPrime =  new PrimeIter();
 			while(itrPrime.hasNext()) {
-				System.out.println(even);
 				BigInteger prime = itrPrime.next();
-				System.out.println(prime);
 				if(even.compareTo(prime) >0) {
 					break;
 				}
@@ -22,14 +21,14 @@ public class main {
 				PrimeIter itrPrime1 =  new PrimeIter();
 				while(itrPrime1.hasNext()) {
 					BigInteger temp = itrPrime.next();
-					if(temp.compareTo(prime) >0) {
+					if(temp.compareTo(even) >0) {
 						break;
 					}
 					if(temp.equals(primecheck)) {
 						System.out.println("checked : "+even+ ","+ "time" +timer);
 					}
 				}
-			}
+			}*/
 		}
 	}
 }
