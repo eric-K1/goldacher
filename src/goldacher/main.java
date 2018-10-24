e8ea63c54aed2c1495c9aa8d06d5dd7d38e33b10
yanivbaldi <yaniv-b@campus.technion.ac.il>
2018-10-24 12:38:14 +0300

    main

--- src/goldacher/main.java
+++ src/goldacher/main.java
@@ -6,29 +6,29 @@ import java.util.Timer;
 
 public class main {
 	public static void main(String [] args) {
-		Timer timer = new Timer();
-		ArithIter itrEven =  new ArithIter(BigInteger.valueOf(0), BigInteger.valueOf(2));
+		ArithIter itrEven =  new ArithIter(BigInteger.valueOf(2), BigInteger.valueOf(2));
 		while(itrEven.hasNext()) {
 			BigInteger even = itrEven.next();
-			System.out.println(even);
-			/*PrimeIter itrPrime =  new PrimeIter();
+			PrimeIter itrPrime =  new PrimeIter();
+			long startTime = System.nanoTime();
 			while(itrPrime.hasNext()) {
 				BigInteger prime = itrPrime.next();
-				if(even.compareTo(prime) >0) {
+				if(even.compareTo(prime) <=0) {
 					break;
 				}
 				BigInteger primecheck = even.subtract(prime);
 				PrimeIter itrPrime1 =  new PrimeIter();
 				while(itrPrime1.hasNext()) {
-					BigInteger temp = itrPrime.next();
-					if(temp.compareTo(even) >0) {
+					BigInteger temp = itrPrime1.next();
+					if(temp.compareTo(primecheck) > 0) {
 						break;
 					}
 					if(temp.equals(primecheck)) {
-						System.out.println("checked : "+even+ ","+ "time" +timer);
+						long endTime = System.nanoTime();
+						System.out.println("checked : "+even+ "=" + prime +"+" +primecheck+ ", time: " + ((endTime - startTime)/1000000000.0) +" seconds.");
 					}
 				}
-			}*/
+			}
 		}
 	}
 }
