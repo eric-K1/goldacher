f9a4ecc9c3f719dd56d50e78166ffc411082d119
ANNY <97anny79@gmail.com>
2018-10-24 11:04:13 +0300

    added arith iter

--- src/goldacher/ArithIter.java
+++ src/goldacher/ArithIter.java
@@ -1,5 +1,28 @@
 package goldacher;
 
-public class ArithIter {
+import java.math.BigInteger;
+import java.util.*;
 
+public class ArithIter implements Iterator<BigInteger>{
+
+	private BigInteger curr;
+	private BigInteger step;
+	
+	public ArithIter(BigInteger base, BigInteger step){
+		this.curr = base;
+		this.step = step;
+	}
+	
+	@Override
+	public boolean hasNext() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public BigInteger next() {
+		// TODO Auto-generated method stub
+		return curr.add(step);
+	}
+	
 }
