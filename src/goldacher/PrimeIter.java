63c18c69f3a769511107f3d8b8c034ba23581ecb
Eric <bla@bla.com>
2018-10-24 11:30:34 +0300

    Updated PrimeIter

--- src/goldacher/PrimeIter.java
+++ src/goldacher/PrimeIter.java
@@ -5,10 +5,24 @@ import java.util.Iterator;
 
 public class PrimeIter implements Iterator<BigInteger> {
 	
-	BigInteger cur = BigInteger.TWO;
+	BigInteger cur = BigInteger.valueOf(2);
+	
+	private static BigInteger sqrt(BigInteger x) {
+	    BigInteger div = BigInteger.ZERO.setBit(x.bitLength()/2);
+	    BigInteger div2 = div;
+	    // Loop until we hit the same value twice in a row, or wind
+	    // up alternating.
+	    for(;;) {
+	        BigInteger y = div.add(x.divide(div)).shiftRight(1);
+	        if (y.equals(div) || y.equals(div2))
+	            return y;
+	        div2 = div;
+	        div = y;
+	    }
+	}
 	
 	private static boolean isPrime(BigInteger p) {
-		BigInteger sqr = p.sqrt().add(BigInteger.ONE);
+		BigInteger sqr = sqrt(q).add(BigInteger.ONE);
 		for(BigInteger q = BigInteger.valueOf(2); q.compareTo(sqr) < 0; q.add(BigInteger.ONE))
 		{
 			if(p.mod(q) == BigInteger.ZERO)
