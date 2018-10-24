764092d0c0aa6810dcfaa635e54fb04acf7c6112
Eric <bla@bla.com>
2018-10-24 12:03:55 +0300

    super good and working PrimeIter

--- src/goldacher/PrimeIter.java
+++ src/goldacher/PrimeIter.java
@@ -4,8 +4,15 @@ import java.math.BigInteger;
 import java.util.Iterator;
 
 public class PrimeIter implements Iterator<BigInteger> {
+
+	private static final BigInteger TWO = BigInteger.valueOf(2);
+	private static final BigInteger THREE = BigInteger.valueOf(3);
+	private static final BigInteger FIVE = BigInteger.valueOf(5);
+	private static final BigInteger SIX = BigInteger.valueOf(6);
 	
-	BigInteger cur = BigInteger.valueOf(2);
+	BigInteger cur = BigInteger.ONE;
+	boolean didAMinus = false;
+	boolean checkedFirsts = false;
 	
 	private static BigInteger sqrt(BigInteger x) {
 	    BigInteger div = BigInteger.ZERO.setBit(x.bitLength()/2);
@@ -22,10 +29,10 @@ public class PrimeIter implements Iterator<BigInteger> {
 	}
 	
 	private static boolean isPrime(BigInteger p) {
-		BigInteger sqr = sqrt(q).add(BigInteger.ONE);
-		for(BigInteger q = BigInteger.valueOf(2); q.compareTo(sqr) < 0; q.add(BigInteger.ONE))
+		BigInteger sqr = sqrt(p).add(BigInteger.ONE);
+		for(BigInteger q = BigInteger.valueOf(2); q.compareTo(sqr) < 0; q=q.add(BigInteger.ONE))
 		{
-			if(p.mod(q) == BigInteger.ZERO)
+			if(p.mod(q).equals(BigInteger.ZERO))
 				return false;
 		}
 		
@@ -41,8 +48,51 @@ public class PrimeIter implements Iterator<BigInteger> {
 	public BigInteger next() {
 		// TODO Currently stupid
 		BigInteger nxt = cur.add(BigInteger.ONE);
-		while(!isPrime(nxt))
-			nxt = nxt.add(BigInteger.ONE);
+		
+		if(!checkedFirsts)
+		{
+			if(cur.compareTo(TWO) < 0)
+			{
+				cur = TWO;
+				return TWO;
+			}
+			if(cur.compareTo(THREE) < 0)
+			{
+				checkedFirsts=true;
+				//cur = THREE;
+
+				
+				cur = SIX;
+				return THREE;
+			}
+			/*if(cur.compareTo(FIVE) < 0)
+			{
+				
+				checkedFirsts=true;
+				cur = FIVE;
+				return FIVE;
+			}*/
+		}
+		
+		boolean found = false;
+		nxt=cur;
+		
+		while(!found)
+		{
+			if(!didAMinus && isPrime(nxt.subtract(BigInteger.ONE)))
+			{
+				cur = nxt;
+				didAMinus = true;
+				return nxt.subtract(BigInteger.ONE);
+			}
+			if(isPrime(nxt.add(BigInteger.ONE)))
+			{
+				cur = nxt.add(SIX);
+				didAMinus = false;
+				return nxt.add(BigInteger.ONE);
+			}
+			nxt = nxt.add(SIX);
+		}
 		
 		BigInteger old = cur;
 		cur = nxt;
